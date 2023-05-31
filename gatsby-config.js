095a8556fbd81835a7ef83d2a54db7c3a9e429e1
Bethany Linquist <beth@MacBook-Pro.local>
2023-05-31 15:51:54 +1000

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,10 @@
 module.exports = {
   siteMetadata: {
     title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
+    
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -7,6 +8,9 @@ const IndexPage = () => {
   
     <main>
         <Layout pageTitle="Home Page">
+          <StaticImage
+          alt="Big ass bitches"
+          src="../images/fallen-angel.jpg"></StaticImage>
       <p>I'm making this by following the Gatsby Tutorial.</p>
       </Layout>
     </main>
